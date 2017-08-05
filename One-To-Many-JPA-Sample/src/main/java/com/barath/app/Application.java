5967d7128d95115b011064cc1d41b8ea2d981ec2
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-08-05 17:42:19 +0530

    added swagger, included different spring profiles

--- One-To-Many-JPA-Sample/src/main/java/com/barath/app/Application.java
+++ One-To-Many-JPA-Sample/src/main/java/com/barath/app/Application.java
@@ -2,12 +2,9 @@ package com.barath.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class,DataSourceAutoConfiguration.class})
-@EnableJpaRepositories
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
